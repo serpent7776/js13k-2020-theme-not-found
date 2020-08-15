4d453fdd83ab788b22a494f327cf650330f251f6
Serpent7776 <serpent7776@gmail.com>
2020-08-15 18:29:18 +0200

    Introduced game state type

--- src/game.ml
+++ src/game.ml
@@ -1,11 +1,15 @@
 open Js_of_ocaml
 module Html = Js_of_ocaml.Dom_html
 
+type state = {
+        theme: string;
+        board: Html.element Js.t;
+}
+
 let log x = Firebug.console##log x
 let id = Fun.id
 let nullstr () = Js.string ""
 
-let theme = "Theme not found"
 let get_board () = Option.get (Html.getElementById_coerce "puzzle" Html.CoerceTo.element)
 
 let todo s = failwith s
@@ -28,7 +32,7 @@ let reset board theme =
         let pattern = String.map f theme in
         board##.innerHTML := Js.string pattern
 
-let reveal_letter str letter =
+let reveal_letter str letter theme =
         let lo = Char.lowercase_ascii in
         let f idx c =
                 if lo theme.[idx] = lo letter then theme.[idx]
@@ -36,26 +40,31 @@ let reveal_letter str letter =
         in
         String.mapi f str
 
-let reveal_board (board: Html.element Js.t) letter =
-        let s = get_text board in
-        let new_s = reveal_letter (Js.to_string s) letter in
-        board##.innerHTML := Js.string new_s
+let reveal_board state letter =
+        let s = get_text state.board in
+        let new_s = reveal_letter (Js.to_string s) letter state.theme in
+        state.board##.innerHTML := Js.string new_s
 
-let theme_found board =
-        let text = get_text board in
-        Js.to_string text = theme
+let theme_found state =
+        let text = get_text state.board in
+        Js.to_string text = state.theme
 
-let keypressed (board: Html.element Js.t) ev =
+let keypressed state ev =
         let key = ev##.key in
         let str = Js.Optdef.case key nullstr id in
         let letter = char_of_js_string str ' ' in
-        reveal_board board letter;
+        reveal_board state letter;
         Js._true
 
 let load _ =
         let board = get_board () in
+        let theme =  "Theme not found" in
+        let state = {
+                theme = theme;
+                board = board;
+        } in
         reset board theme;
-        Html.document##.onkeydown := Html.handler (keypressed board);
+        Html.document##.onkeydown := Html.handler (keypressed state);
         Js._false
 
 let _ =
